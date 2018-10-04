93ae880f2515bb2a381ced60cc56aac8e6326daf
Niels van Eeden <Niels.van.eeden@sogeti.nl>
2018-10-04 20:31:50 +0200

    imagepuller now rights to dev instead of prod

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -14,7 +14,7 @@ echo "Setting up Parks Production Environment in project ${GUID}-parks-prod"
 
 # To be Implemented by Student
 oc policy add-role-to-user view --serviceaccount=default -n $GUID-parks-prod
-oc policy add-role-to-group system:image-puller system:serviceaccounts:$GUID-parks-prod -n $GUID-parks-prod
+oc policy add-role-to-group system:image-puller system:serviceaccounts:$GUID-parks-prod -n $GUID-parks-dev
 oc policy add-role-to-user edit system:serviceaccount:$GUID-jenkins:jenkins -n $GUID-parks-prod
 oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n $GUID-parks-prod
 
